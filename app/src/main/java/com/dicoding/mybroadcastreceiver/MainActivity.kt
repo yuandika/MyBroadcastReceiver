e53ac9a7e8255d0da1267817d8d3fabc105abfcc
Yuandika Alfahreiza <yuandika11@gmail.com>
2021-12-12 09:17:19 +0700

    BroadcastReceiver dari custom Event

--- app/src/main/java/com/dicoding/mybroadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/dicoding/mybroadcastreceiver/MainActivity.kt
@@ -7,10 +7,17 @@ import android.view.View
 import android.widget.Toast
 import com.dicoding.mybroadcastreceiver.databinding.ActivityMainBinding
 import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.util.Log
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private var binding: ActivityMainBinding? = null
+    private lateinit var downloadReceiver: BroadcastReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,11 +25,26 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(binding?.root)
 
         binding?.btnPermission?.setOnClickListener(this)
+        binding?.btnDownload?.setOnClickListener(this)
+
+        downloadReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                Log.d(DownloadService.TAG, "download selesai")
+                Toast.makeText(context, "download selesai", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val downloadIntentFilter = IntentFilter(ACTION_DOWNLOAD_STATUS)
+        registerReceiver(downloadReceiver, downloadIntentFilter)
     }
 
     override fun onClick(v: View) {
-        when {
-            v.id == R.id.btn_permission -> PermissionManager.check(this, Manifest.permission.RECEIVE_SMS, SMS_REQUEST_CODE)
+        when (v.id){
+            R.id.btn_permission -> PermissionManager.check(this, Manifest.permission.RECEIVE_SMS, SMS_REQUEST_CODE)
+            R.id.btn_download -> {
+                val downloadServiceIntent = Intent(this, DownloadService::class.java)
+                startService(downloadServiceIntent)
+            }
         }
     }
 
@@ -42,11 +64,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onDestroy() {
         super.onDestroy()
-
+        unregisterReceiver(downloadReceiver)
         binding == null
     }
 
     companion object {
         private const val SMS_REQUEST_CODE = 101
+        const val ACTION_DOWNLOAD_STATUS = "download_status"
     }
 }
